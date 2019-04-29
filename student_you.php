6e0b986c17c95ad28a70e61012727e877039229a
Alexander <mrreads@yandex.com>
2019-04-30 00:38:28 +0300

    student_you db view info

--- student_you.php
+++ student_you.php
@@ -6,6 +6,39 @@ if (empty($_SESSION['id_user']))
     header('Location: index.php');
 }
 
+$user_id = $_SESSION['id_user'];
+
+//ВОЗЬМЁМ ИНФОРМАЦИ О СТУДЕНТЕ, ЧТОБЫ ПОЗЖЕ ЕЁ ВЫВЕСТИ.
+require_once 'php/connection.php';
+$query_user_info = "SELECT
+                            fio,
+                            date_of_birth,
+                            number_group,
+                            form_study,
+                            email,
+                            phone_number,
+                            name_role
+                    FROM 
+                            students,
+                            groups,
+                            roles
+                    WHERE 
+                            students.id_group = groups.id_group
+                    AND
+                            students.id_role = roles.id_role
+                    AND
+                            id_student = '$user_id';";
+
+$result_user_info = mysqli_query($link, $query_user_info);
+$data_user_info = mysqli_fetch_row($result_user_info);
+
+$user_name = $data_user_info[0];
+$user_birth = $data_user_info[1];
+$user_group = $data_user_info[2];
+$user_formstudy = $data_user_info[3];
+$user_email = $data_user_info[4];
+$user_phone = $data_user_info[5];
+$user_role = $data_user_info[6];
 ?>
 
 <!DOCTYPE html>
@@ -68,35 +101,29 @@ if (empty($_SESSION['id_user']))
                 <div id="profile">
                     <img src="images/avatar.jpg">
                     <div class='p-background'>
-                        <h2 class="b-name"> Имя Фамилия </h2>
-                        <p class="b-info"> Студент </p>
+            <? echo "<h2 class='b-name'> $user_name </h2>"; ?>
+            <? echo "<p class='b-info'> $user_role </p>"; ?>
                     </div>
                     <div class='info-background'>
                         <div class="user-info">
                             <p> Имя ученика: </p>
-                            <p> [ваше фио] </p>
+                    <? echo "<p> $user_name</p>"; ?>
                             <hr>
                             <p> Дата рождения: </p>
-                            <p> [ваша дата рождения] </p>
-                            <hr>
-                            <p> Специальность: </p>
-                            <p> <a href="student_sceciality_profile.php"> [ваша специальность] </a> </p>
+                    <? echo "<p> $user_birth </p>"; ?>
                             <hr>
                             <p> Группа: </p>
-                            <p> <a href="student_group_profile.php"> [ваша группа] </a> </p>
+                    <? echo "<p> <a href='student_group_profile.php'> $user_group </a> </p>"; ?> 
                             <hr>
                             <p> Форма обучения: </p>
-                            <p> [ваша форма обучения] </p>
+                    <? echo "<p> $user_formstudy </p>"; ?>
                         </div>
                         <div class="profile-info">
                             <p> Почтовый адрес: </p>
-                            <p> [ваша почта] </p>
+                    <? echo "<p> $user_email </p>"; ?>
                             <hr>
                             <p> Номер: </p>
-                            <p> [если указан] </p>
-                            <hr>
-                            <p> ВКонтакте : </p>
-                            <p> <a href=""> [если указан] </a> </p>
+                    <? echo "<p> $user_phone </p>"; ?>
                         </div>
                     </div>
                 </div>
