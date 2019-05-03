81e82948ca3ac825db6a5e6cb787f36063c8b543
Alexander <mrreads@yandex.com>
2019-05-03 14:25:53 +0300

    auth

--- php/auth.php
+++ php/auth.php
@@ -11,17 +11,21 @@ $email_auth = $_POST['email-auth'];
 $password_auth = $_POST['password-auth'];
 $button_auth = $_POST['button-auth'];
 
-$query_login = "SELECT email, password, id_student FROM students";
+$query_login = "SELECT id_role, id_users FROM users WHERE email = '$email_auth' AND password = '$password_auth'";
 $result_login = mysqli_query($link, $query_login);
+$data_login = mysqli_fetch_row($result_login);
 
-while ($row = mysqli_fetch_row($result_login)) 
+
+if ($data_login[0] == 2)
 {
-    if (($email_auth == $row[0]) and ($password_auth == $row[1])) 
-    {
-        $_SESSION['id_user'] = "$row[2]";
-        header('Location: ../index.php');
-    }
+    $query_auth = "SELECT id_student FROM students WHERE id_user = '$data_login[1]'";
+    $result_auth = mysqli_query($link, $query_auth);
+    $data_auth = mysqli_fetch_row($result_auth);
+    $_SESSION['id_user'] = "$data_auth[0]";
+    header('Location: ../index.php');
 }
+        
+
 
 if (empty($_SESSION['id_user'])) 
 {
